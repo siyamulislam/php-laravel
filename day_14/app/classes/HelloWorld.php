036f7861ebc771f9e397c9d1685e19767000bd0f
siam-cse <siyamidt@gmail.com>
2022-10-24 17:34:56 +0600

    day-14 php hellow world

--- day_14/app/classes/HelloWorld.php
+++ day_14/app/classes/HelloWorld.php
@@ -6,5 +6,13 @@ namespace App\classes;
 
 class HelloWorld
 {
+    public $message;
+    public function __construct()
+    {
+        $this->message='hello World';
+    }
 
+    public function index(){
+        echo $this->message;
+    }
 }
