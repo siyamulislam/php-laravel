a6a5c27190b2fca47be99d089b642562e837a812
siyamulislam <siyamulislam1@gmail.com>
2022-11-15 00:42:43 +0600

    header footer dynamic

--- day_29/laravel-day2/resources/views/home.blade.php
+++ day_29/laravel-day2/resources/views/home.blade.php
@@ -1,35 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
-    <link rel="stylesheet" href="{{asset('/')}}assets/css/bootstrap.css">
-</head>
-<body>
-
-
-{{--<ul>--}}
-{{--    <li><a href="{{ url('/') }}">Home</a></li>--}}
-{{--    <li><a href="{{ url('/about') }}">About</a> </li>--}}
-{{--    <li><a href="{{ url('/contact') }}">Contact</a></li>--}}
-{{--</ul>--}}
-
-<nav class="navbar navbar-expand-md bg-dark navbar-dark">
-    <div class="container">
-        <a href="" class="navbar-brand">LOGO</a>
-        <ul  class="navbar-nav">
-            <li><a class="nav-link" href="{{ route('home') }}">      Home</a></li>
-            <li><a class="nav-link" href="{{ route('about') }}">     About</a> </li>
-            <li><a class="nav-link" href="{{ route('contact') }}">   Contact</a></li>
-        </ul>
-    </div>
-
-</nav>
+@include('includes/header')
 <h1>This is Home Page</h1>
-
-<script src="{{asset('/')}}assets/js/bootstrap.bundle.css"></script>
-</body>
-</html>
+@include('includes/footer')
