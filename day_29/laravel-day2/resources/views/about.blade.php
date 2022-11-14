e6852cc08133efb03386d9d4fb3a45caff8fe2a4
siam-cse <siyamidt@gmail.com>
2022-11-14 15:59:59 +0600

    basic url routing

---
+++ day_29/laravel-day2/resources/views/about.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body>
+{{--<ul>--}}
+{{--    <li><a href="{{ route('home') }}">Home</a></li>--}}
+{{--    <li><a href="{{ route('about') }}">About</a></li>--}}
+{{--    <li><a href="{{ route('contact') }}">Contact</a></li>--}}
+{{--</ul>--}}
+
+<ul>
+    <li><a href="{{ url('/') }}">Home</a></li>
+    <li><a href="{{ url('/about') }}">About</a></li>
+    <li><a href="{{ url('/contact') }}">Contact</a></li>
+</ul>
+<h1>This is About Page</h1>
+</body>
+</html>
--- day_29/laravel-day2/routes/web.php
+++ day_29/laravel-day2/routes/web.php
@@ -2,17 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+//Route::get('/home',     [HomeController::class,'home']);
+
+//Route::get('/',         [HomeController:: class,'home'])        ->name('home');
+//Route::get('/about',    [HomeController:: class,'about'])       ->name('about');
+//Route::get('/contact',  [HomeController:: class,'contact'])     ->name('contact');
+
+Route::get('/',         [HomeController:: class,'home'])        ;
+Route::get('/about',    [HomeController:: class,'about'])     ;
+Route::get('/contact',  [HomeController:: class,'contact'])    ;
