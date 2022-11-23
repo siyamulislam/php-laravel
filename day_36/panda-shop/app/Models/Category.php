461b6faf81fa6861ba101925d504224648f0df0c
siam-cse <siyamidt@gmail.com>
2022-11-23 14:42:26 +0600

    category added

---
+++ day_36/panda-shop/app/Models/Category.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+    private static $category, $image, $imageExtension, $imageName, $directory, $imageUrl;
+
+    private static function getImageUrl($request)
+    {
+        self::$image = $request->file('image');
+        self::$imageExtension = self::$image->getClientOriginalExtension();
+//        self::$imageName = time().'.'.self::$imageExtension;
+        self::$imageName = time().'_'.self::$image->getClientOriginalName();
+        self::$directory = 'category-images/';
+        self::$image->move(self::$directory, self::$imageName);
+        return self::$directory.self::$imageName;
+    }
+
+    public static function addCategory($request)
+    {
+        self::$category = new Category();
+        self::$category->name = $request->name;
+        self::$category->description = $request->description;
+        self::$category->image = self::getImageUrl($request);
+        self::$category->save();
+    }
+
+    public static function updateCategory($request,$id)
+    {
+        self::$category = Category::find($id);
+        if($request->file('image'))
+        {
+            if(file_exists(self::$category->image))
+            {
+                unlink(self::$category->image);
+            }
+            self::$imageUrl = self::getImageUrl($request);
+        }
+        else
+        {
+            self::$imageUrl = self::$category->image;
+        }
+
+        self::$category->name = $request->name;
+        self::$category->description = $request->description;
+        self::$category->image = self::$imageUrl;
+        self::$category->save();
+    }
+
+    public static function deleteCategory($id)
+    {
+        self::$category = Category::find($id);
+        if(file_exists(self::$category->image))
+        {
+            unlink(self::$category->image);
+        }
+        self::$category->delete();
+    }
+}
--- day_36/panda-shop/routes/web.php
+++ day_36/panda-shop/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',                 [HomeController::class,'index'])->name('home');
+Route::get('/category/add',     [CategoryController::class,'index'])->name('category.add');
+Route::post('/category/store',  [CategoryController:: class,"store"])   ->name('category.store');
+
+Route::get('/category/manage',          [CategoryController::class,'manage'])->name('category.manage');
+Route::get('/category/edit/{id}',       [CategoryController::class, 'edit'])->name('category.edit');
+Route::post('/category/update/{id}',    [CategoryController::class, 'update'])->name('category.update');
+Route::get('/category/delete/{id}',     [CategoryController::class, 'delete'])->name('category.delete');
+
