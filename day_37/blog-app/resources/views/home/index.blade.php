7ce102f77be81cfae9516859123f0efd82b16785
siyamulislam <siyamulislam1@gmail.com>
2022-11-27 23:38:44 +0600

    blog view chenaged

--- day_37/blog-app/resources/views/home/index.blade.php
+++ day_37/blog-app/resources/views/home/index.blade.php
@@ -7,17 +7,18 @@
 
     <section class="py-5">
         <div class="container">
+            <div class="text-center"> <h1>Blogs</h1> </div>
             <div class="row">
                     @foreach($blogs as $blog)
                 <div class="col-md-3">
-                    <div class="card blog-card h-400 my-4    ">
-                        <img src="{{$blog['image']}}" alt="" height="162" class="card-img hover-zoom">
+                    <div class="card blog-card shadow h-400 my-4 border-0">
+                        <img src="{{$blog['image']}}" alt="" height="162" class="card-img ">
                         <div class="card-body my-1">
                             <h6 class="text-secondary fw-normal ">#{{$blog->category['name']}}</h6>
-                            <h5 class="fw-semibold my-3  ">
+                            <h4 class="fw-semibold my-3  ">
                                 <a href="{{ route('blog.detail', ['id'=>$blog->id]) }}"
                                    class="text-decoration-none text-dark  ">{{ $blog['title']}}</a>
-                            </h5>
+                            </h4>
                             <small>{{$blog->created_at}} | <span>3min ago</span></small>
                         </div>
                     </div>
