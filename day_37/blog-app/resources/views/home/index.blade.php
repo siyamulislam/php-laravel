5f2d5cce8673afd4c57581280dd8c70b4620040e
siyamulislam <siyamulislam1@gmail.com>
2022-11-30 00:07:30 +0600

    nav pill init

--- day_37/blog-app/resources/views/home/index.blade.php
+++ day_37/blog-app/resources/views/home/index.blade.php
@@ -4,30 +4,72 @@
 @endsection
 
 @section('body')
-    <section class="py-5">
-        <div class="container">
-            <div class="text-center"> <h1>Blogs</h1> </div>
+    <section class="py-3">
+        <div class="container-fluid">
             <div class="row">
-                    @foreach($blogs as $blog)
-{{--                    @php($lastUpdate=\App\Http\Controllers\HomeController::getLastUpdateDate($blog))@endphp--}}
+                <div class="col-md-12">
+                    <div>
+                        <ul class="nav nav-tabs">
+                            <li><a href="" class="nav-link active" data-bs-toggle="tab" data-bs-target="#all">All</a></li>
+                            @foreach($categories as $category)
+                                <li><a href="#{{$category->name}}" class="nav-link" data-bs-toggle="tab">{{$category->name}}</a></li>
+                                @endforeach
 
-                    <div class="col-xl-3 col-lg-4 col-sm-6">
-                    <div class="card blog-card shadow h-400 my-4 border-0">
-                        <a href="{{ route('home.detail', ['id'=>$blog->id]) }}">
-                            <img src="{{$blog['image']}}" alt=""  height="162" class="card-img "></a>
-                        <div class="card-body my-1">
-                            <h6 class="text-secondary fw-normal ">#{{$blog->category['name']}}</h6>
-                            <h4 class="fw-semibold my-3 ">
-                                <a href="{{ route('home.detail', ['id'=>$blog->id]) }}"
-                                   class="text-decoration-none text-dark  ">{{ $blog['title']}}</a>
-                            </h4>
-                            <small>
-                                {{ \App\Http\Controllers\HomeController::getLastUpdateDate($blog).  " ago" }}</small>
+                        </ul>
+                        <div class="tab-content">
+                            <div class="tab-pane fade show active" id="all">
+                                    <div class="container">
+                                        <div class="row">
+                                            @foreach($blogs as $blog)
+                                                <div class="col-xl-3 col-lg-4 col-sm-6">
+                                                    <div class="card blog-card shadow h-400 my-4 border-0">
+                                                        <a href="{{ route('home.detail', ['id'=>$blog->id]) }}">
+                                                            <img src="{{$blog['image']}}" alt=""  height="162" class="card-img "></a>
+                                                        <div class="card-body my-1">
+                                                            <h6 class="text-secondary fw-normal ">#{{$blog->category['name']}}</h6>
+                                                            <h4 class="fw-semibold my-3 ">
+                                                                <a href="{{ route('home.detail', ['id'=>$blog->id]) }}"
+                                                                   class="text-decoration-none text-dark  ">{{ $blog['title']}}</a>
+                                                            </h4>
+                                                            <small>{{ \App\Http\Controllers\HomeController::getLastUpdateDate($blog)." ago" }}</small>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                            </div>
+
+                            @foreach($categories as $category)
+                                <div class="tab-pane show fade" id={{$category->name}}>
+                                    <h3>{{$category->name}}</h3>
+                                    <div class="container">
+                                        <div class="row">
+                                            @foreach($blogs as $blog)
+                                                <div class="col-xl-3 col-lg-4 col-sm-6">
+                                                    <div class="card blog-card shadow h-400 my-4 border-0">
+                                                        <a href="{{ route('home.detail', ['id'=>$blog->id]) }}">
+                                                            <img src="{{$blog['image']}}" alt=""  height="162" class="card-img "></a>
+                                                        <div class="card-body my-1">
+                                                            <h6 class="text-secondary fw-normal ">#{{$blog->category['name']}}</h6>
+                                                            <h4 class="fw-semibold my-3 ">
+                                                                <a href="{{ route('home.detail', ['id'=>$blog->id]) }}"
+                                                                   class="text-decoration-none text-dark  ">{{ $blog['title']}}</a>
+                                                            </h4>
+                                                            <small>{{ \App\Http\Controllers\HomeController::getLastUpdateDate($blog)." ago" }}</small>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
                         </div>
                     </div>
                 </div>
-                @endforeach
             </div>
         </div>
     </section>
+
 @endsection
