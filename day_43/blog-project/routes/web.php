5ff2762fea3235353fd9e17b12829d88b50e2be8
siam-cse <siyamidt@gmail.com>
2022-12-04 15:44:52 +0600

    ustra mastering 20%

--- day_43/blog-project/routes/web.php
+++ day_43/blog-project/routes/web.php
@@ -1,21 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/',                 [FrontController::class, 'home'])->                 name('home');
+Route::get('/home',                 [FrontController::class, 'home'])->                 name('home');
+Route::get('/product-details',  [FrontController::class, 'productDetails'])->       name('product-details');
 
 Route::middleware([
     'auth:sanctum',
