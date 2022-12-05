ad3620fd4d57a1cc81417de788a94d248cc462e7
siam-cse <siyamidt@gmail.com>
2022-12-05 14:58:02 +0600

    admin mastering

---
+++ day_43/blog-project/resources/views/admin/includes/css.blade.php
@@ -0,0 +1,10 @@
+<!-- App favicon -->
+<link rel="shortcut icon" href="{{asset('/')}}admin/assets/images/favicon.ico">
+
+<!-- third party css -->
+<link href="{{asset('/')}}admin/assets/css/vendor/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css"/>
+<!-- third party css end -->
+
+<!-- App css -->
+<link href="{{asset('/')}}admin/assets/css/icons.min.css" rel="stylesheet" type="text/css"/>
+<link href="{{asset('/')}}admin/assets/css/app.min.css" rel="stylesheet" type="text/css" id="app-style"/>
---
+++ day_43/blog-project/resources/views/admin/includes/js.blade.php
@@ -0,0 +1,14 @@
+
+<!-- bundle -->
+<script src="{{asset('/')}}admin/assets/js/vendor.min.js"></script>
+<script src="{{asset('/')}}admin/assets/js/app.min.js"></script>
+
+<!-- third party js -->
+<script src="{{asset('/')}}admin/assets/js/vendor/apexcharts.min.js"></script>
+<script src="{{asset('/')}}admin/assets/js/vendor/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="{{asset('/')}}admin/assets/js/vendor/jquery-jvectormap-world-mill-en.js"></script>
+<!-- third party js ends -->
+
+<!-- demo app -->
+<script src="{{asset('/')}}admin/assets/js/pages/demo.dashboard.js"></script>
+<!-- end demo js-->
