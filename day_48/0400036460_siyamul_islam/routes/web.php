81fea99831c55b100c0824451c9cf5d5719251af
siam-cse <siyamidt@gmail.com>
2022-12-11 15:12:54 +0600

    dashboard mastering

--- day_48/0400036460_siyamul_islam/routes/web.php
+++ day_48/0400036460_siyamul_islam/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FrontController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,7 +24,5 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get ('/dashboard',               [DashboardController::class, 'dashboard'])->name('dashboard');
 });
