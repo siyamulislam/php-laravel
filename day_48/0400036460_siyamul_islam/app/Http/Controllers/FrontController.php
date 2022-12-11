eb99d94185f94b83d65c1169a2c3a0440258772d
siam-cse <siyamidt@gmail.com>
2022-12-11 14:31:59 +0600

    front mastering

---
+++ day_48/0400036460_siyamul_islam/app/Http/Controllers/FrontController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DateTime;
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    public function home(){
+//        $products = Product::where('status',1)->orderBy('id', 'DESC')->get();
+//        $brands = Brand::where('status',1)->orderBy('id', 'DESC')->get();
+//       print_r($category);
+//        return view ('front.home.home', ['products' => $products,'brands'=>$brands]);
+        return view ('front.home.home');
+    }
+
+    public function productDetails($id){
+        $this->product = Product::find($id);
+        $lastUpdate = $this->getLastUpdateDate($this->product);
+        $this->product['lastUpdate']=$lastUpdate;
+        return view ('front.product.details', ['product' => $this->product]);
+    }
+
+
+    public static function getLastUpdateDate($blog)
+    {
+        $dateDiff = date_diff(new DateTime(), $blog->updated_at);
+
+        $dateFormat = '';
+        if ($dateDiff->format('%y') != 0) $dateFormat = $dateFormat . '%y year';
+        if ($dateDiff->format('%m') != 0) $dateFormat = $dateFormat . ' %m month';
+        if ($dateDiff->format('%d') != 0) $dateFormat = $dateFormat . ' %d day';
+        if ($dateDiff->format('%H') != 0) $dateFormat = $dateFormat . ' %Hh';
+        if ($dateDiff->format('%I') != 0) $dateFormat = $dateFormat . ' %Im';
+        if ($dateDiff->format('%S') != 0) $dateFormat = $dateFormat . ' %Ss';
+        $lastUpdate = $dateDiff->format($dateFormat);
+        return $lastUpdate;
+    }
+}
--- day_48/0400036460_siyamul_islam/routes/web.php
+++ day_48/0400036460_siyamul_islam/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FrontController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',                         [FrontController::class, 'home'])->                 name('home');
+Route::get('/home',                     [FrontController::class, 'home'])->                 name('home');
+//Route::get('/products/{id}',      [FrontController::class, 'productDetails'])->             name('products');
 
 Route::middleware([
     'auth:sanctum',
