982c00a33992e05cfb1e3c7dc5665431f82cd570
siam-cse <siyamidt@gmail.com>
2022-12-20 16:44:41 +0600

    clock

--- day_55/js/script.js
+++ day_55/js/script.js
@@ -1,38 +1,52 @@
+// var clock = document.getElementById('clock').value;
+setInterval( getTime, 1000);
+function getTime(){
+    var today= new Date();
+    var hour= today.getHours();
+    var min= today.getMinutes();
+    var sec= today.getSeconds();
+    var ampm = hour  >= 12 ? 'pm' : 'am';
+    hour = hour % 12;
+    var time= hour+':'+min+':'+sec +' '+ampm
+    document.getElementById('clock').innerText=time;
+}
 
- var reset= document.getElementById('reset');
+// Calculator
+var reset = document.getElementById('reset');
+var divi = document.getElementById('divi');
+var multi = document.getElementById('multi');
+var sub = document.getElementById('sub');
+var add = document.getElementById('add');
 
- var divi= document.getElementById('divi');
- var multi= document.getElementById('multi');
- var sub= document.getElementById('sub');
- var add= document.getElementById('add');
-
- divi.onclick = ()=>calculator('/');
- multi.onclick = ()=>calculator('*');
- sub.onclick = ()=>calculator('-');
- add.onclick = ()=>calculator('+');
+divi.onclick = () => calculator('/');
+multi.onclick = () => calculator('*');
+sub.onclick = () => calculator('-');
+add.onclick = () => calculator('+');
 
 
- reset.onclick=()=>resetCal();
- function calculator(op){
-    var fn= Number(document.getElementById('firstNumber').value);
-    var ln= Number(document.getElementById('lastNumber').value);
-    var result=0;
-    switch(op){
-        case '+':result=fn+ln; break;
-        case '-':result=fn-ln;break;
-        case '*':result=fn*ln;break;
-        case '/':result=fn/ln;break;
-    } 
-    document.getElementById('result').value=result;
- }
+reset.onclick = () => resetCal();
+function calculator(op) {
+    var fn = Number(document.getElementById('firstNumber').value);
+    var ln = Number(document.getElementById('lastNumber').value);
+    var result = 0;
+    switch (op) {
+        case '+': result = fn + ln; break;
+        case '-': result = fn - ln; break;
+        case '*': result = fn * ln; break;
+        case '/': result = fn / ln; break;
+    }
+    document.getElementById('result').value = result;
+}
 
- function resetCal(){
-  document.getElementById('firstNumber').value='';
-  document.getElementById('lastNumber').value='';
-  document.getElementById('result').value='';
-  document.getElementById('divi').checked = false;
-  document.getElementById('multi').checked = false;
-  document.getElementById('sub').checked = false;
-  document.getElementById('add').checked = false; 
+function resetCal() {
+    // var operator = document.querySelector('input[name="operator"]:checked').value;
+    // alert(operator);
+    document.getElementById('firstNumber').value = '';
+    document.getElementById('lastNumber').value = '';
+    document.getElementById('result').value = '';
+    document.getElementById('divi').checked = false;
+    document.getElementById('multi').checked = false;
+    document.getElementById('sub').checked = false;
+    document.getElementById('add').checked = false;
 
- }
+}
