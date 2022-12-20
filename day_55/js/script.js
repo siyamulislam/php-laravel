5cbe4787afdaf33244de11179e0b9fb852e260d5
siam-cse <siyamidt@gmail.com>
2022-12-20 15:13:15 +0600

    calculator

--- day_55/js/script.js
+++ day_55/js/script.js
@@ -1,1 +1,38 @@
- 
+
+ var reset= document.getElementById('reset');
+
+ var divi= document.getElementById('divi');
+ var multi= document.getElementById('multi');
+ var sub= document.getElementById('sub');
+ var add= document.getElementById('add');
+
+ divi.onclick = ()=>calculator('/');
+ multi.onclick = ()=>calculator('*');
+ sub.onclick = ()=>calculator('-');
+ add.onclick = ()=>calculator('+');
+
+
+ reset.onclick=()=>resetCal();
+ function calculator(op){
+    var fn= Number(document.getElementById('firstNumber').value);
+    var ln= Number(document.getElementById('lastNumber').value);
+    var result=0;
+    switch(op){
+        case '+':result=fn+ln; break;
+        case '-':result=fn-ln;break;
+        case '*':result=fn*ln;break;
+        case '/':result=fn/ln;break;
+    } 
+    document.getElementById('result').value=result;
+ }
+
+ function resetCal(){
+  document.getElementById('firstNumber').value='';
+  document.getElementById('lastNumber').value='';
+  document.getElementById('result').value='';
+  document.getElementById('divi').checked = false;
+  document.getElementById('multi').checked = false;
+  document.getElementById('sub').checked = false;
+  document.getElementById('add').checked = false; 
+
+ }
